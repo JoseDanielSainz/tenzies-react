68ec90d2407ff7a42de938104eece6b284683977
Jose Sainz <jose.sainz@clickbalance.com>
2025-07-11 17:52:25 -0700

    feat: Added full functionality to game

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { useState } from 'react'
 import Die from './Die'
 import { nanoid } from 'nanoid'
 
+interface DieType {
+  value: number,
+  isHeld: boolean,
+  id: string
+}
+
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice())
+  const [rollCount, setRollCount] = useState(0)
 
   function generateAllNewDice() {
     return new Array(10)
@@ -15,13 +22,43 @@ export default function App() {
       }))
   }
 
-  console.log(generateAllNewDice())
 
-  function rollDices() {
-    setDice(generateAllNewDice())
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die =>
+      die.isHeld === false ?
+        { ...die, value: Math.ceil(Math.random() * 6) } :
+        die
+    ))
+    setRollCount(oldCount => oldCount + 1)
   }
 
-  const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />)
+  function newGame() {
+    setDice(generateAllNewDice())
+    setRollCount(0)
+  }
+
+  function hold(id: string) {
+    setDice(oldDice => oldDice.map(die =>
+      die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    ))
+  }
+
+  let gameWon = false
+
+  dice.every(die => die.value === dice[0].value) &&
+    dice.every(die => die.isHeld === true)
+    ? gameWon = true : null
+
+  const diceElements = dice.map(dieObj => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
+  ))
 
   return (
     <main>
@@ -32,7 +69,16 @@ export default function App() {
         {diceElements}
 
       </div>
-      <button onClick={rollDices} className='roll-btn'>Roll</button>
+
+      {gameWon === false
+        ? <button onClick={rollDice} className='roll-btn'>Roll</button>
+        :
+        <div>
+          <button onClick={newGame} className="new-game-btn">New Game</button>
+          <div className="win-text">You completed the game in {rollCount} rolls!</div>
+        </div>
+      }
+
     </main>
   )
 }
--- src/Die.tsx
+++ src/Die.tsx
@@ -1,6 +1,12 @@
 export default function Die(props: any) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
 
   return (
-    <button className={props.isHeld ? 'die held-die' : 'die'}>{props.value}</button>
+    <button
+      style={styles}
+      onClick={props.hold}
+    >{props.value}</button>
   )
 }
